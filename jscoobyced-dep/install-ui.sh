afb65a7182409446d0b911837cd1346337b33448
Cedric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-02-14 17:09:57 +0700

    Add more packages

--- jscoobyced-dep/install-ui.sh
+++ jscoobyced-dep/install-ui.sh
@@ -21,4 +21,6 @@ install_repo() {
 
 install_repo "Brave Browser" "brave-browser" "https://brave-browser-apt-release.s3.brave.com/brave-core.asc" "https://brave-browser-apt-release.s3.brave.com/ stable main" "brave-browser"
 
+install_repo "Spotify" "spotify" "https://download.spotify.com/debian/pubkey.gpg" "deb http://repository.spotify.com stable non-free" "spotify-client"
+
 echo "Installation complete."
--- jscoobyced-dep/uninstall-ui.sh
+++ jscoobyced-dep/uninstall-ui.sh
@@ -5,4 +5,9 @@ rm -f /etc/apt/sources.list.d/brave-browser.list
 rm -f /usr/share/keyrings/brave-browser.gpg
 rm -f /etc/apt/trusted.gpg.d/brave-browser-release.gpg
 
+echo "Removing Spotify repository."
+rm -f /etc/apt/sources.list.d/spotify.list
+rm -f /usr/share/keyrings/spotify.gpg
+rm -f /etc/apt/trusted.gpg.d/spotify-*.gpg
+
 echo "Uninstallation complete."
