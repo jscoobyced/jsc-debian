82c532dceef23570f8c48d1e407159bbe8a6bb0c
Cédric Rochefolle <19742131+jscoobyced@users.noreply.github.com>
2021-10-24 14:33:33 +0700

    Refactor and add support for media dependencies.

--- jscoobyced-dep/build-all.sh
+++ jscoobyced-dep/build-all.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+rm -Rf jscoobyced-dep*
+
 ./build.sh
 ./build.sh ui
 ./build.sh dev
+./build.sh media
+
 rm -f ../../jscoobyced.github.io/repo/amd64/jscoobyced-dep*.deb
 mv jscoobyced-dep*.deb ../../jscoobyced.github.io/base/
---
+++ jscoobyced-dep/deps.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+install_repo() {
+  echo "Installing $1 repository."
+  ALREADY=$(add-apt-repository -L | grep -i $2 | wc -l)
+
+  if [ "0" = "${ALREADY}" ]; then
+    curl -fsSL $3 | gpg --dearmor | tee /usr/share/keyrings/$2.gpg > /dev/null
+    echo "deb [arch=amd64 signed-by=/usr/share/keyrings/$2.gpg] $4" | tee /etc/apt/sources.list.d/$2.list
+    echo ""
+    echo "##########################################################################"
+    echo "# You will need to run:"
+    echo "# sudo apt update && sudo apt install $5"
+    echo "# in order to install $1"
+    echo "##########################################################################"
+    echo ""
+  else
+    echo "Already setup."
+  fi
+}
